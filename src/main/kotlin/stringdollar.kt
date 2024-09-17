91f0886c492423869805c94f3af65da6fc4cd87b
H P <nurchumairohahmad@gmail.com>
2024-09-17 09:29:38 +0700

    Stater project

--- src/main/kotlin/stringdollar.kt
+++ src/main/kotlin/stringdollar.kt
@@ -1,10 +1,11 @@
 fun main(args: Array<String>) {
+    //string template
     val a = 7
     val b = 8
     val c = a + b
     val s = "When we add $a and $b, we get $c"
     println(s)
-    println("When we add $a and $b, we get ${a + b}")
+    println("When we add $a and $b, we get ${a + b}") //literal string (tanda kutip 2)
     val price = """
         ${'$'}9.99
         """
